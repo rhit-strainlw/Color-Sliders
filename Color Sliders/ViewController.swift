245bc7daa01636964175a8348ab0124d296a3b52
Loki Strain <strainlw@rose-hulman.edu>
2021-06-08 12:52:18 -0400

    Initial Commit

---
+++ Color Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Sliders
+//
+//  Created by Loki Strain on 6/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
